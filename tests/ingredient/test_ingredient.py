e3ad2e47812bc1fe03d97d4a906a1c1696a6338f
jeferson-viana <jefersonv28@gmail.com>
2023-10-05 15:37:40 -0300

    feat: Implementando os testes da classe Ingredient (Requisito 1 concluído)

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,15 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Ingredient, Restriction  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredient_1 = Ingredient("queijo parmesão")
+    ingredient_2 = Ingredient("queijo parmesão")
+    ingredient_3 = Ingredient("queijo provolone")
+    assert ingredient_1.name == "queijo parmesão"
+    assert hash(ingredient_1) == hash(ingredient_2)
+    assert hash(ingredient_1) != hash(ingredient_3)
+    assert ingredient_1.restrictions == {Restriction.ANIMAL_DERIVED,
+                                         Restriction.LACTOSE}
+    assert ingredient_1.__repr__() == "Ingredient('queijo parmesão')"
+    assert ingredient_1 == ingredient_2
