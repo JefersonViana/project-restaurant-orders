2c989b1ba3b25c03a9f8eceb58c1097de675cc19
jeferson-viana <jefersonv28@gmail.com>
2023-10-05 22:52:52 -0300

    feat: Implementando a classe MenuData (Requisito 3 concluído)

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,27 @@
-# Req 3
+from models.dish import Dish, Ingredient
+import csv
+from abc import abstractmethod
+
+
 class MenuData:
     def __init__(self, source_path: str) -> None:
-        pass
+        self.dishes = set()
+        exist_dish = {}
+        for dish in self.read_file(source_path):
+            if dish[0] in exist_dish:
+                exist_dish[dish[0]].add_ingredient_dependency(
+                        Ingredient(dish[2]), int(dish[3])
+                    )
+            else:
+                exist_dish[dish[0]] = Dish(dish[0], float(dish[1]))
+                exist_dish[dish[0]].add_ingredient_dependency(
+                    Ingredient(dish[2]), int(dish[3])
+                )
+        for dish in exist_dish.values():
+            self.dishes.add(dish)
+
+    @abstractmethod
+    def read_file(self, source_path):
+        with open(source_path, encoding="utf-8") as file:
+            header, *data = csv.reader(file, delimiter=",", quotechar='"')
+        return data
